e83d9191d9e154169ac5db1760246818eae54957
Aline Pereira <aline.pereira@liferay.com>
2023-04-11 00:48:03 -0300

    Projeto completo todo

---
+++ src/components/firstComponent.js
@@ -0,0 +1,11 @@
+function FirstComponent(){
+    return(
+        <div className="firstcomponent">
+            <p>Primeiro Componente</p>
+           
+        </div>
+    )
+
+}
+
+export default FirstComponent
